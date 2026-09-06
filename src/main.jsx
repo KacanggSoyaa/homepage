eabe9c2d7fb6072de8386c83676a480dc9b9bbf2
KacanggSoyaa <danisnazri08@gmail.com>
2026-09-07 04:21:39 +0800

    Fix Router basename for custom domain: /homepage -> /

--- src/main.jsx
+++ src/main.jsx
@@ -35,7 +35,7 @@ const sections = [
 //   - The `sections` array is passed into <App> so it knows what to render on "/".
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/homepage">
+    <BrowserRouter basename="/">
       <App sections={sections} />
     </BrowserRouter>
   </React.StrictMode>,
